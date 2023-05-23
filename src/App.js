cc0280402cd02d30bfce5b454b706fc520a01e6a
ThangLyo <quocthangisme@gmail.com>
2023-05-23 16:03:59 +0700

    Functional Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/Navigation';
+import Player from './components/Player';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-     <h1> 
-      Thang
-     </h1>
+     <Navigation/> 
+      <Player/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+export default class Navigation extends Component {
+    render() {
+        return (
+            <div className='footer'>
+                <p className='ft'>
+                    copyright © 2022
+                </p>
+            </div>
+        )
+    }
+}
